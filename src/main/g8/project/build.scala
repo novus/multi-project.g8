62a2eba97d753abc33fb7defbb124fe53af7d651
Chris Lewis <clewis@novus.com>
2012-12-24 16:55:46 -0500

    test deps, test config (junit xml), compiler options

--- src/main/g8/project/build.scala
+++ src/main/g8/project/build.scala
@@ -23,7 +23,10 @@ object $name;format="Camel,word"$Build extends sbt.Build {
 object Shared {
     
   val testDeps = Seq(
-    "org.specs2" %% "specs2" % "1.12.3" % "test"
+    "org.specs2" %% "specs2" % "1.12.3" % "test",
+    "org.mockito" % "mockito-all" % "1.9.0" % "test",
+    "org.scalacheck" %% "scalacheck" % "1.9" % "test",
+    "junit" % "junit" % "4.7" % "test"
   )
 
   val settings = Seq(
@@ -31,6 +34,9 @@ object Shared {
     version := "$version$",
     scalaVersion := "2.9.2",
     crossScalaVersions := Seq("2.9.1"),
+    scalacOptions := Seq("-deprecation", "-unchecked"),
+    javacOptions ++= Seq("-source", "1.5", "-target", "1.5"),
+    testOptions in Test += Tests.Argument("console", "junitxml"),
     resolvers ++= Seq("snapshots" at "http://oss.sonatype.org/content/repositories/snapshots",
                       "releases"  at "http://oss.sonatype.org/content/repositories/releases"),
     initialCommands := "import $organization$.$name;format="lower,word"$._",
